a8a97c402cf2b8ff5a07c2df0f634512c1d4662c
cbossie <cbossie@gmail.com>
2019-11-22 21:48:02 -0500

    Add Presentation

---
+++ src/DotNetServerless/LambdaEntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace DotNetServerless
+{
+    public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<Startup>();
+        }
+    }
+}
